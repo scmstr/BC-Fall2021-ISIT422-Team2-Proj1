d7146366d68553dd2058276380ddba14d49d3e7c
scmstr <scmstr@hotmail.com>
2021-10-01 13:20:20 -0700

    finished adding first html page

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+//test! blank page at basic empty {port}. sends message in single quotes as html string/content
+// app.get('/', (req, res) => res.send('potatoes for everybody - it works!'))
+
+
+//tells you that it's working. ITS ALIIIIIIIIIIIIIVE!!!! (its october, have fun >:D)
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+
+
+
+
+//ask for nothing, literally sends you the page1.html file thats in the "__dirname" location
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/page1.html')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
